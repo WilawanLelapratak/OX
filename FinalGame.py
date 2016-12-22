2241daa9c4eeb858f1beee8aa33ebc323fdc181d
WilawanLelapratak <wilawan.wove@gmail.com>
2016-12-22 13:06:20 +0700

    Final Game V : Fix Bugs Thanks for watching Subscribe for next part Just Kidding It is over

--- FinalGame.py
+++ FinalGame.py
@@ -22,13 +22,13 @@ class Board :
                                      '03': 13, '13': 14, '23': 15, '33' : 16}
         self.container = tkinter.Frame(self.parent)
         self.container.pack()
-        self.canvas = tkinter.Canvas(self.container, width = self.sq_size * 4, height = sel.sq_size * 4)
+        self.canvas = tkinter.Canvas(self.container, width = self.sq_size * 4, height = self.sq_size * 4)
         self.canvas.grid()
 
     def get_unused_squares_dict(self) :
         return self.unused_squares_dict
 
-    def reste_unused_squares_dict(self) :
+    def reset_unused_squares_dict(self) :
         self.unused_squares_dict = { '00': 1, '10': 2, '20': 3, '30' : 4,
                                      '01': 5, '11': 6, '21': 7, '31' : 8,
                                      '02': 9, '12': 10, '22': 11, '32' : 12,
@@ -37,36 +37,41 @@ class Board :
     def draw_board(self) :
         for row in range(4) :
             for(column) in range(4) :
-                self.canvas.create_rectangle(self.sq_size * column, self.sq_size * row, self.sq_size * (column + 1), self.sq_size * (row + 1), fill self.color)
+                self.canvas.create_rectangle(self.sq_size * column, self.sq_size * row, self.sq_size * (column + 1), self.sq_size * (row + 1), fill = self.color)
 
-        def get_row_col(self, evt) :
-            return evt.x, evt.y
+    def get_row_col(self, evt) :
+        return evt.x, evt.y
 
-        def floor_of_row_col(self, col, rw) :
-            col_flr = col // self.sq_size
-            rw_flr = rw // self.sq_size
-            return col_flr, rw_flr
+    def floor_of_row_col(self, col, rw) :
+        col_flr = col // self.sq_size
+        rw_flr = rw // self.sq_size
+        return col_flr, rw_flr
 
-        def convert_to key(self, col_floor, row_floor) :
-            return str(col_floor) + str(row_floor)
+    def convert_to_key(self, col_floor, row_floor) :
+        return str(col_floor) + str(row_floor)
 
-        def find_coords_of_selcted_sq(self, evt) :
-            column, row = self.get_row_col(evt)
-            column_floor, row_floor = self.floor_of_row_col(column, row)
-            rowcol_key_str = self.convert_to_key(column_floor, row_floor)
-            corner_column = (column_floor * self.sq_size) + self.sq_size
-            corner_row = (row_floor * self.sq_size) + self.sq_size
-            return corner_column, corner_row
+    def find_coords_of_selected_sq(self, evt):
+        column, row = self.get_row_col(evt)
+        column_floor, row_floor = self.floor_of_row_col(column, row)
+        rowcol_key_str = self.convert_to_key(column_floor, row_floor)
+        corner_column = (column_floor * self.sq_size) + self.sq_size
+        corner_row =  (row_floor  * self.sq_size) + self.sq_size
+        return corner_column, corner_row
 
-        def color_selected_sq(self, evt, second_corner_col, second_corner_row, player_color) :
-            self.canvas.create_rectangle((evt.x // sself.sq_size) * self.sq_size, (evt.y // self.sq_size) * self.sq_size, second_corner_col, second_corner_row, fill = player_color)
+    def color_selected_sq(self, evt, second_corner_col, second_corner_row, player_color) :
+        self.canvas.create_rectangle(
+            (evt.x // self.sq_size) * self.sq_size,
+            (evt.y // self.sq_size) * self.sq_size,
+            second_corner_col,
+            second_corner_row,
+            fill = player_color)
 
-        @property
-        def victory_ways(self) :
-            retrun self._victory_ways
+    @property
+    def victory_ways(self) :
+        return self._victory_ways
 
 class GamePlay(object) :
-    def __ init__(self, parent) :
+    def __init__(self, parent) :
         self.parent = parent
         self.board = Board(self.parent, 100, "#ECECEC")
         self.board.draw_board()
@@ -78,7 +83,7 @@ class GamePlay(object) :
 
     def initialize_buttons(self) :
         self.two_players_button = tkinter.Button(self.board.container, text = "TWO PLAYERS", width = 25, command = self.init_two_players_game)
-        self.restart_button  = tkinter.Button(self.board.container, text = "RESTART", wodth = 25, command = self.restart)
+        self.restart_button  = tkinter.Button(self.board.container, text = "RESTART", width = 25, command = self.restart)
 
     def show_menu(self) :
         self.two_players_button.grid()
@@ -100,13 +105,13 @@ class GamePlay(object) :
 
     def add_to_player_sq(self, key, player_sq) :
         current_selected_sq = self.board.unused_squares_dict[key]
-        player_ssq.add(current_selected_sq)
+        player_sq.add(current_selected_sq)
 
     def delete_used_sq(self, key) :
         del self.board.unused_squares_dict[key]
 
     def play(self, event) :
-        colrow_tuple = self.board.find_coords_of_selcted_sq(event)
+        colrow_tuple = self.board.find_coords_of_selected_sq(event)
         corner_two_col, corner_two_row = colrow_tuple[0], colrow_tuple[1]
         col_fl, row_fl = self.board.floor_of_row_col(event.x, event.y)
         rowcol_key = self.board.convert_to_key(col_fl, row_fl)
@@ -117,7 +122,7 @@ class GamePlay(object) :
             return
 
         if self.player1_turn == True :
-            self.add_to_player_sq(rowcol_key, self.player1,selected_sq)
+            self.add_to_player_sq(rowcol_key, self.player1.selected_sq)
             self.delete_used_sq(rowcol_key)
             self.board.color_selected_sq(event, corner_two_col, corner_two_row, self.player1.color)
             self.check_for_winner(self.player1.selected_sq, self.player1.name)
@@ -134,7 +139,7 @@ class GamePlay(object) :
         if len(self.board.unused_squares_dict) > 0 :
             if len(player_sq) > 2 :
                 for combo in permutations(player_sq, 4) :
-                    for wc in self.board.winning_combos :
+                    for wc in self.board.victory_ways :
                         if set(combo) == wc :
                             self.show_game_result(player_name + " WIN!")
                             self.restart
